82453d67f56dc0f845bcb6897b7490c560b78e79
Ambuj03 <mishraambuj8269@gmail.com>
2025-01-29 18:09:03 +0530

    resolved some errors(operational error, integrity error) by passing some pseudo value in the form, but next task will to fetch and put real values into it)

--- bm_app/forms.py
+++ bm_app/forms.py
@@ -1,20 +1,65 @@
 from django import forms
 
-from .models import Books
+from .models import Books, Receipt, Customer, Donation
+
 
 class transaction_form(forms.Form):
     book = forms.ModelChoiceField(queryset=Books.objects.all(), label = 'Select a book',
             to_field_name='book_name')
 
     quantity = forms.IntegerField()
-    donation = forms.IntegerField()
+    donation_amount = forms.IntegerField()
+    donation_purpose = forms.CharField()
     customer_name = forms.CharField()
-    customer_phone = forms.NumberInput()
+    customer_phone = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel'}))
     customer_occupation = forms.CharField()
+    customer_city = forms.CharField()
     remarks = forms.CharField()
 
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs): #study object oriented programming to understand this
         super(transaction_form, self).__init__(*args, **kwargs)
         # import pdb; pdb.set_trace()
         self.fields['book'].label_from_instance = lambda obj: obj.book_name
+
+    
+    def save(self):
+        data = self.cleaned_data
+        # saving customer dataa
+
+        customer = Customer(
+            # customer_id = data['customer_id'],
+            customer_name=data['customer_name'],
+            customer_phone=data['customer_phone'],
+            customer_occupation=data['customer_occupation'],
+            customer_city=data['customer_city'],
+            customer_remarks=data['remarks']
+        )
+
+        customer.save()
+
+        #updating the donations table
+
+        donation = Donation(
+            customer_id = 1,
+            donation_amount = data['donation_amount'],
+            donation_purpose = data['donation_purpose']
+        )
+
+        donation.save()
+
+        #save receipt data
+        receipt = Receipt(
+            customer_id = 1, #try to declare a seperate function.
+            book_id = 1, # fetched from bokk table referenced by book name
+            distributor_id = 1, # will come through authorization, make login
+            book=data['book'],
+            quantity=data['quantity'],
+            # donation=data['donation_amount'],
+            # customer_name=data['customer_name'],
+            # customer_phone=data['customer_phone'],
+            # customer_occupation=data['customer_occupation'],
+            # remarks=data['remarks'], doesnt exists in the original table
+            donation = donation,
+            customer = customer
+        )
+        receipt.save()
