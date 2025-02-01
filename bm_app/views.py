422bc3b402ae1bf87ac1f9ec2dbcf9931e41e755
Ambuj03 <mishraambuj8269@gmail.com>
2025-02-01 15:43:37 +0530

    Added a form for login and wrote this view but encountering some issues

--- bm_app/views.py
+++ bm_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import transaction_form
+from .forms import transaction_form, login_form
+
+# login required things from documentation
+from django.contrib.auth import authenticate,login
+
 
 #Showing main page
 def main_page(request):
@@ -35,8 +39,26 @@ def new_transaction_view(request):
         # }
     return render(request, 'bm_app/new_transaction.html', {'form' : my_form})
 
+
 def login_view(request):
-    return render(request = )
+
+    if request.method == 'POST':
+        my_login_form = login_form()
+        if my_login_form.is_valid():
+            username = my_login_form.cleaned_data.get('username')
+            password = my_login_form.cleaned_data.get('password')
+            user = authenticate(request, username = username, password = password)
+            if user is not None :
+                login(request, user)
+                return redirect("bm_app/new_transaction.html")
+            else:
+                login_form.add_error(None,'Invalid Username or Password')
+                return redirect("bm_app/login.html")
+
+
+    else:
+       my_login_form = login_form()
+       return render(request,"bm_app/login.html", {'form' : my_login_form})
 
 
 
