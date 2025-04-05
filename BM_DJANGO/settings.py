b14f93552230928b0618a8bb26c2ea4b610d6aa4
Ambuj03 <mishraambuj8269@gmail.com>
2025-04-05 10:54:54 +0530

    Added celery configs in settings

--- BM_DJANGO/settings.py
+++ BM_DJANGO/settings.py
@@ -181,3 +181,14 @@ JAZZMIN_SETTINGS = {
 }
 
 
+# Celery Configuration
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'  # Use your timezone
+
+# Django Celery Beat Settings
+INSTALLED_APPS += ['django_celery_beat']
+
